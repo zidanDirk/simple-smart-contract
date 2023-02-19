3d320747c834f854d28ddaa36576c32a1715ff14
zhangzidan <zhangzidan@huya.com>
2023-02-19 20:55:49 +0800

    feat: update

--- contracts/StudentListStorage.sol
+++ contracts/StudentListStorage.sol
@@ -8,6 +8,7 @@ contract StudentListStorage {
         uint id;
         string name;
         uint age;
+        address account;
     }
     // 动态数组
 
@@ -16,7 +17,7 @@ contract StudentListStorage {
     function addList(string memory _name, uint _age) public returns (uint) {
         uint count = StudentList.length;
         uint index = count + 1;
-        StudentList.push(Student(index, _name, _age));
+        StudentList.push(Student(index, _name, _age, msg.sender));
         return StudentList.length;
     }
 
